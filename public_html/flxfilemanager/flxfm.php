ec3988ba5c52a7cfe58af852a8f466f192eae678
alg <alg.info@gmail.com>
2011-09-16 12:26:18 +0900

    flxfilemanager

---
+++ public_html/flxfilemanager/flxfm.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>flxfilemanager</title>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script type="text/javascript">
+$(function(){
+	alert('hoge');
+})
+</script>
+</head>
+<body>
+<?php
+	if( isset($_POST["crontab"]) ){
+		$crontab = $_POST["crontab"];
+		$crontab = str_replace("\r\n","\n",$crontab);
+		if( substr($crontab,-1) != "\n" ){
+			$crontab = $crontab."\n";
+		}
+		$ct = fopen("../../.crontab","w");
+		@fwrite($ct,$crontab,strlen($crontab));
+
+		fclose($ct);
+		print trim( htmlspecialchars( shell_exec("/usr/bin/crontab ../../.crontab") ) );
+		print "<br>";
+	}
+?>
+	<form method="POST">
+	<textarea name="crontab" rows="10" cols="60"><?php
+	if( file_exists("../../.crontab") ){
+		$ct = fopen("../../.crontab", "r");
+		print fread($ct,filesize("../../.crontab"));
+		fclose($ct);
+	}else{
+		touch("../../.crontab");
+		chmod("../../.crontab",0604);
+	}
+?></textarea>
+	<input type="submit" value="submit">
+	</form>
+</body>
+</html>
